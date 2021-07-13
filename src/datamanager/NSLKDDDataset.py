fixed energy average issue

src/datamanager/NSLKDDDataset.py
@@ -7,13 +7,16 @@ import pandas as pd
 import numpy as np
 from sklearn import preprocessing
 
+# TODO
+# Refactor this class
+#
 
 class NSLKDDDataset(Dataset):
     """
     This class is used to load KDD Cup dataset as a pytorch Dataset
     """
 
-    def __init__(self, path=None):
+    def __init__(self, path='../data/kddcup_data'):
         self.path = path
 
         # load data
@@ -22,10 +25,9 @@ class NSLKDDDataset(Dataset):
         # else:
         #     data = self._load_data(path)
 
-        # data = self._load_data(path)
-        # self.X, self.y = data[:, :-1], data[:, -1]
-        df = self.import_raw(path)
-        X, y = self.preprocess(df)
+        data = self._load_data(path)
+        self.X, self.y = data[:, :-1], data[:, -1]
+
         # Normalize data
         # X_mean = np.mean(self.X, axis=1)
         # X_std = np.std(self.X, axis=1)
@@ -33,7 +35,7 @@ class NSLKDDDataset(Dataset):
 
         # scaler = preprocessing.MinMaxScaler()
         # self.X = scaler.fit_transform(self.X)
-        self.n = len(X)
+        self.n = len(data)
 
     def __len__(self):
         return len(self.X)

src/model/DAGMM.py
@@ -15,7 +15,7 @@ class DAGMM(nn.Module):
     Simply put, it's an end-to-end trained auto-encoder network complemented by a distinct gaussian mixture network.
     """
 
-    def __init__(self, input_size, ae_layers=None, gmm_layers=None, lambda_1=0.1, lambda_2=0.005):
+    def __init__(self, input_size, ae_layers=None, gmm_layers=None, lambda_1=0.1, lambda_2=0.005, device='cpu'):
         """
         DAGMM constructor
 
@@ -42,7 +42,7 @@ class DAGMM(nn.Module):
         self.K = None
         self.lambda_1 = lambda_1
         self.lambda_2 = lambda_2
-        self.device = 'cpu'
+        self.device = device
 
     def forward(self, x: torch.Tensor):
         """
@@ -56,9 +56,9 @@ class DAGMM(nn.Module):
         #   - :math:`z_c = h(x; \theta_e)`
         #   - :math:`z_r = f(x, x')`
         code = self.ae.encoder(x)
-        x_hat = self.ae.decoder(code)
-        rel_euc_dist = self.relative_euclidean_dist(x, x_hat)
-        cosim = self.cosim(x, x_hat)
+        x_prime = self.ae.decoder(code)
+        rel_euc_dist = self.relative_euclidean_dist(x, x_prime)
+        cosim = self.cosim(x, x_prime)
         z_r = torch.cat([code, rel_euc_dist.unsqueeze(-1), cosim.unsqueeze(-1)], dim=1)
 
         # compute gmm net output, that is
@@ -67,7 +67,7 @@ class DAGMM(nn.Module):
         gamma_hat = self.gmm.forward(z_r)
         # gamma = self.softmax(output)
 
-        return code, x_hat, cosim, z_r, gamma_hat
+        return code, x_prime, cosim, z_r, gamma_hat
 
     def weighted_log_sum_exp(self, x, weights, dim):
         """
@@ -122,8 +122,8 @@ class DAGMM(nn.Module):
 
         # K x D
         # :math: `\mu = (I * gamma_sum)^{-1} * (\gamma^T * z)`
-        # mu = torch.sum(gamma.unsqueeze(-1) * z.unsqueeze(1), dim=0) / gamma_sum.unsqueeze(-1)
-        mu = torch.linalg.inv(torch.diag(gamma_sum)) @ (gamma.T @ z)
+        mu = torch.sum(gamma.unsqueeze(-1) * z.unsqueeze(1), dim=0) / gamma_sum.unsqueeze(-1)
+        # mu = torch.linalg.inv(torch.diag(gamma_sum)) @ (gamma.T @ z)
 
         # Covariance (K x D x D)
         covs = []
@@ -165,7 +165,7 @@ class DAGMM(nn.Module):
             energies.append(-self.weighted_log_sum_exp(torch.log(probs), phi[k]))
         return torch.sum(torch.Tensor(energies))
 
-    def estimate_sample_energy(self, z, phi=None, mu=None, cov_mat=None, average_it=True, device='cpu'):
+    def estimate_sample_energy(self, z, phi=None, mu=None, cov_mat=None, average_energy=True, device='cpu'):
 
         if phi is None:
             phi = self.phi
@@ -205,7 +205,7 @@ class DAGMM(nn.Module):
         # energy computation
         energy_result = - max_val.squeeze() - torch.log(log_term + eps)
 
-        if average_it:
+        if average_energy:
             energy_result = energy_result.mean()
 
         # penalty term
@@ -273,8 +273,6 @@ class DAGMM(nn.Module):
 
         """
         rec_err = ((x - x_prime) ** 2).mean()
-        # rec_err = ((x - x_prime) ** 2).sum(1)
-        N = x.shape[0]
-        loss = rec_err + (self.lambda_1 / N) * energy + self.lambda_2 * pen_cov_mat
+        loss = rec_err + self.lambda_1 * energy + self.lambda_2 * pen_cov_mat
 
         return loss

src/model/GMM.py
@@ -7,7 +7,7 @@ import torch
 class GMM(nn.Module):
     def __init__(self, layers=None):
         super(GMM, self).__init__()
-        default_layers = [(3, 10, nn.Tanh()), (None, None, nn.Dropout(0.5)), (10, 4, nn.Softmax())]
+        default_layers = [(3, 10, nn.Tanh()), (None, None, nn.Dropout(0.5)), (10, 4, nn.Softmax(dim=-1))]
         self.net = self.create_network(layers or default_layers)
 
     def create_network(self, layers: List[Tuple]):

src/trainer/DAGMMTrainTestManager.py
@@ -31,6 +31,7 @@ class DAGMMTrainTestManager:
             warnings.warn("CUDA is not available. Suppress this warning by passing "
                           "use_cuda=False to {}()."
                           .format(self.__class__.__name__), RuntimeWarning)
+            print('\n\n')
             device_name = 'cpu'
 
         self.device = torch.device(device_name)
@@ -191,7 +192,7 @@ class DAGMMTrainTestManager:
                 # forward pass
                 code, x_prime, cosim, z, gamma = self.model(train_inputs)
                 sample_energy, pen_cov_mat = self.model.estimate_sample_energy(
-                    z, train_phi, train_mu, train_cov, average_it=False, device=self.device
+                    z, train_phi, train_mu, train_cov, average_energy=False, device=self.device
                 )
 
                 train_energy.append(sample_energy.cpu().numpy())
@@ -210,7 +211,7 @@ class DAGMMTrainTestManager:
                 # forward pass
                 code, x_prime, cosim, z, gamma = self.model(test_inputs)
                 sample_energy, pen_cov_mat = self.model.estimate_sample_energy(
-                    z, train_phi, train_mu, train_cov, average_it=False, device=self.device
+                    z, train_phi, train_mu, train_cov, average_energy=False, device=self.device
                 )
                 test_energy.append(sample_energy.cpu().numpy())
                 test_z.append(z.cpu().numpy())