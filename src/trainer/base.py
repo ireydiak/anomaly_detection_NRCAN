1f7f3ea1f84cff6cd35ee35fc32a593fccef0700
D'Jeff Nkashama Kanda <djeff.nkashama.kanda@usherbrooke.ca>
2022-03-31 11:57:56 -0400

    added SAD preprocessing

--- src/trainer/base.py
+++ src/trainer/base.py
@@ -24,6 +24,7 @@ class BaseTrainer(ABC):
         self.n_jobs_dataloader = n_jobs_dataloader
         self.n_epochs = n_epochs
         self.lr = lr
+        self.weight_decay = kwargs.get('weight_decay', 0)
         self.optimizer = self.set_optimizer(weight_decay=kwargs.get('weight_decay', 0))
 
     @abstractmethod
--- src/trainer/transformers.py
+++ src/trainer/transformers.py
@@ -1,5 +1,5 @@
 import torch
-from torch import nn
+from torch import nn, optim
 from torch.optim.lr_scheduler import StepLR
 from src.trainer.base import BaseTrainer
 
@@ -9,6 +9,13 @@ class NeuTraLADTrainer(BaseTrainer):
     def __init__(self, **kwargs):
         super(NeuTraLADTrainer, self).__init__(**kwargs)
         self.metric_hist = []
+
+        mask_params = list()
+        for mask in self.model.masks:
+            mask_params += list(mask.parameters())
+        self.optimizer = optim.Adam(list(self.model.enc.parameters()) + mask_params, lr=self.lr,
+                                    weight_decay=self.weight_decay)
+
         self.scheduler = StepLR(self.optimizer, step_size=20, gamma=0.9)
         self.criterion = nn.MSELoss()
 
