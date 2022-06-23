df0b109cd91361b253b139712037d0e8fbdbc2e6
Jean-Charles Verdier <jean-charles.verdier@usherbrooke.ca>
2022-06-23 16:32:40 -0400

    Rebase

--- pyad/lightning.py
+++ pyad/lightning.py
@@ -1,25 +1,81 @@
 from pyad.model.reconstruction import LitAutoEncoder
 from pyad.datamanager.dataset import ThyroidDataset, ArrhythmiaDataset
-from pyad.model.transformers import LitGOAD
+import pyad.model.transformers
+from pyad.model.transformers import LitGOAD, LitNeuTraLAD
+import pyad.datamanager.data_module
+from pyad.datamanager.data_module import BaseDataset
 import pytorch_lightning as pl
+from pytorch_lightning.utilities.cli import MODEL_REGISTRY, DATAMODULE_REGISTRY, LightningCLI
+from pytorch_lightning.utilities.cli import MODEL_REGISTRY, DATAMODULE_REGISTRY
+from jsonargparse import ArgumentParser
+import argparse
+
+MODEL_REGISTRY.register_classes(pyad.model.transformers, pl.LightningModule)
+DATAMODULE_REGISTRY.register_classes(pyad.datamanager.data_module, pl.LightningDataModule)
 
 
-def main():
-    # data_path = "../data/Thyroid/thyroid.mat"
-    data_path = "../data/Arrhythmia/arrhythmia_normalized.npz"
-    #dataset = ThyroidDataset(path=data_path)
-    dataset = ArrhythmiaDataset(path=data_path)
-    batch_size = 64
+class MyLightningCLI(LightningCLI):
+    def add_arguments_to_parser(self, parser):
+        # parser.add_class_arguments(BaseDataset, "dataset")
+        parser.link_arguments("data.in_features", "model.init_args.in_features", apply_on="instantiate")
+        parser.link_arguments("data.n_instances", "model.init_args.n_instances", apply_on="instantiate")
+
+
+class VerboseStore(argparse.Action):
+    def __init__(self, option_strings, dest, nargs=None, **kwargs):
+        if nargs is not None:
+            raise ValueError('nargs not allowed')
+        super(VerboseStore, self).__init__(option_strings, dest, **kwargs)
+
+    def __call__(self, parser, namespace, values, option_string=None):
+        print('Here I am, setting the ' \
+              'values %r for the %r option...' % (values, option_string))
+        model = MODEL_REGISTRY[values]
+        model.add_model_specific_args(parser)
+        # for model in MODEL_REGISTRY.classes:
+        #     model.add_model_specific_args(parser)
+        #     parser.add_class_arguments(model, "model")
+        # setattr(namespace, self.dest, values)
+
+
+def argparser():
+    parser = ArgumentParser()
+   # parser = pl.Trainer.add_argparse_args(parser)
+    parser.add_argument("--model", type=str)
+    parser.add_argument("--dataset", type=str)
+    # model-specific arguments
+    # for model in MODEL_REGISTRY.classes:
+    #     model.add_model_specific_args(parser)
+    # parser.link_arguments("data.in_features", "model.init_args.in_features", apply_on="instantiate")
+    # parser.link_arguments("data.n_instances", "model.init_args.n_instances", apply_on="instantiate")
+    return parser.parse_args()
+
+
+def prepare_thyroid(model_name: str):
+    model_name = model_name.lower()
+    data_path = "../data/Thyroid/thyroid.mat"
+    dataset = ThyroidDataset(path=data_path)
+    batch_size = 128
     train_ldr, test_ldr, _ = dataset.loaders(batch_size=batch_size)
 
-    model_name = "goad"
-    if model_name.lower() == "autoencoder":
+    if model_name == "litautoencoder":
         model = LitAutoEncoder(
             in_features=dataset.in_features,
             n_instances=dataset.n_instances,
             latent_dim=2,
             hidden_dims=[4],
             activation="relu"
+        )
+    elif model_name == "litneutralad":
+        model = LitNeuTraLAD(
+            in_features=dataset.in_features,
+            weight_decay=1e-4,
+            lr=1e-3,
+            n_transforms=11,
+            trans_type="mul",
+            temperature=0.1,
+            trans_hidden_dims=[24],
+            enc_hidden_dims=[24, 24, 24, 24, 24]
         )
     else:
         model = LitGOAD(
@@ -33,8 +89,63 @@ def main():
             lamb=0.1,
             margin=1
         )
+    return model, train_ldr, test_ldr
+
+
+def prepare_arrhythmia(model_name: str):
+    model_name = model_name.lower()
+    data_path = "../data/Arrhythmia/arrhythmia_normalized.npz"
+    dataset = ArrhythmiaDataset(path=data_path)
+    batch_size = 64
+    if model_name.lower() == "litautoencoder":
+        model = LitAutoEncoder(
+            in_features=dataset.in_features,
+            n_instances=dataset.n_instances,
+            latent_dim=2,
+            hidden_dims=[4],
+            activation="relu"
+        )
+    elif model_name == "litneutralad":
+        model = LitNeuTraLAD(
+            in_features=dataset.in_features,
+            weight_decay=1e-4,
+            lr=1e-3,
+            n_transforms=11,
+            trans_type="mul",
+            temperature=0.1,
+            trans_hidden_dims=[200, 274],
+            enc_hidden_dims=[64, 64, 64, 64, 32]
+        )
+    else:
+        model = LitGOAD(
+            in_features=dataset.in_features,
+            n_transforms=256,
+            feature_dim=32,
+            num_hidden_nodes=8,
+            batch_size=batch_size,
+            n_layers=0,
+            eps=0,
+            lamb=0.1,
+            margin=1
+        )
+    train_ldr, test_ldr, _ = dataset.loaders(batch_size=batch_size)
+
+    return model, train_ldr, test_ldr
+
+
+def main(args):
+    dataset_name = args.dataset.lower()
+    max_epochs = 50
     # trainer
-    trainer = pl.Trainer(precision=16, accelerator="gpu", devices=1, max_epochs=200)
+    trainer = pl.Trainer(precision=16, accelerator="gpu", devices=1, max_epochs=max_epochs)
+
+    if dataset_name == "arrhythmia":
+        model, train_ldr, test_ldr = prepare_arrhythmia(args.model)
+    elif dataset_name == "thyroid":
+        model, train_ldr, test_ldr = prepare_thyroid(args.model)
+    else:
+        model, train_ldr, test_ldr = prepare_thyroid(args.model)
+
     # learn
     trainer.fit(
         model=model,
@@ -45,16 +156,11 @@ def main():
         model=model,
         dataloaders=test_ldr
     )
-    # scores, y_true, _ = trainer.predict(
-    #     model=model,
-    #     dataloaders=test_ldr,
-    # )
-    # evaluate
-
-    #y_test_true, test_scores, _ = trainer.test(test_ldr)
-    #results, _ = metrics.score_recall_precision_w_threshold(test_scores, y_test_true)
-    a = 1
 
 
+# LightningArgumentParser
 if __name__ == "__main__":
-    main()
+    # cli = MyLightningCLI()
+    main(
+        argparser()
+    )
--- pyad/model/transformers.py
+++ pyad/model/transformers.py
@@ -1,11 +1,17 @@
+from typing import Union, Optional, Callable, Any, List
+from pytorch_lightning.utilities.cli import MODEL_REGISTRY, DATAMODULE_REGISTRY
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
+from torch.optim import Optimizer
+from torch.optim.lr_scheduler import StepLR
 
 from pyad.loss.TripletCenterLoss import TripletCenterLoss
 from pyad.model.base import BaseModel
 import pytorch_lightning as pl
+from pytorch_lightning.core.optimizer import LightningOptimizer
 
 from pyad.utils import metrics
 
@@ -34,29 +40,17 @@ class NeuTraLAD(BaseModel):
 
     def __init__(
             self,
-            # fc_1_out: int,
-            # fc_last_out: int,
-            # compression_unit: int,
             n_transforms: int,
-            # n_layers: int,
             trans_type: str,
             temperature: float,
-            # trans_fc_in: int,
-            # trans_fc_out: int,
             trans_hidden_dims: list,
             enc_hidden_dims: list,
             **kwargs
     ):
         super(NeuTraLAD, self).__init__(**kwargs)
-        # self.compression_unit = compression_unit
-        # self.fc_1_out = fc_1_out
-        # self.fc_last_out = fc_last_out
-        # self.n_layers = n_layers
         self.n_transforms = n_transforms
         self.temperature = temperature
         self.trans_type = trans_type
-        # self.trans_fc_in = trans_fc_in if trans_fc_in and trans_fc_in > 0 else self.in_features
-        # self.trans_fc_out = trans_fc_out if trans_fc_out and trans_fc_out > 0 else self.in_features
         self.cosim = nn.CosineSimilarity()
         # Encoder and Transformation layers
         self.enc_hidden_dims = enc_hidden_dims
@@ -69,19 +63,14 @@ class NeuTraLAD(BaseModel):
         self.masks = self._create_masks()
         # self._build_network()
 
-    #
     @staticmethod
     def get_args_desc():
         return [
-            ("fc_1_out", int, 90, "output dim of first hidden layer"),
-            ("fc_last_out", int, 32, "output dim of the last layer"),
-            ("compression_unit", int, 20, "used to set output dim of next layer (in_feature - compression_unit)"),
             ("temperature", float, 0.07, "temperature parameter"),
             ("trans_type", str, "mul", "transformation type (choose between 'res' or 'mul')"),
-            ("n_layers", int, 4, "number of layers"),
             ("n_transforms", int, 11, "number of transformations"),
-            ("trans_hidden_dims", list, [200], "dimensions of the hidden layers"),
-            ("enc_hidden_dims", list, [64, 32], "dimensions of the encoder layers")
+            ("trans_hidden_dims", str, "200", "dimensions of the hidden layers"),
+            ("enc_hidden_dims", list, "64,64,64,64,32", "dimensions of the encoder layers")
         ]
 
     def _create_masks(self):
@@ -93,30 +82,6 @@ class NeuTraLAD(BaseModel):
                 nn.Sequential(*layers).to(self.device)
             )
         return masks
-
-    def _create_masks_legacy(self) -> list:
-        masks = [None] * self.n_transforms
-        out_dims = self.trans_layers
-        for K_i in range(self.n_transforms):
-            net_layers = create_network(self.in_features, out_dims, bias=False)
-            net_layers[-1] = nn.Sigmoid()
-            masks[K_i] = nn.Sequential(*net_layers).to(self.device)
-        return masks
-
-    def _build_network(self):
-        out_dims = [0] * self.n_layers
-        out_features = self.fc_1_out
-        for i in range(self.n_layers - 1):
-            out_dims[i] = out_features
-            out_features -= self.compression_unit
-        out_dims[-1] = self.fc_last_out
-        self.trans_layers = [self.trans_fc_in, self.trans_fc_out]
-
-        # Encoder
-        enc_layers = create_network(self.in_features, out_dims)[:-1]  # removes ReLU from the last layer
-        self.enc = nn.Sequential(*enc_layers).to(self.device)
-        # Masks / Transformations
-        self.masks = self._create_masks_legacy()
 
     @staticmethod
     def load_from_ckpt(ckpt):
@@ -213,6 +178,176 @@ class NeuTraLAD(BaseModel):
         return self.score(X)
 
 
+@MODEL_REGISTRY
+class LitNeuTraLAD(pl.LightningModule):
+    name = "LITNeuTraLAD"
+
+    def __init__(
+            self,
+            in_features: int,
+            weight_decay: float,
+            lr: float,
+            n_transforms: int,
+            trans_type: str,
+            temperature: float,
+            trans_hidden_dims: List[int],
+            enc_hidden_dims: List[int],
+    ):
+        super(LitNeuTraLAD, self).__init__()
+        # call this to save hyper-parameters to the checkpoint
+        self.save_hyperparameters(
+           ignore=["in_features"]
+        )
+        # General parameters
+        self.in_features = in_features
+        self.weight_decay = weight_decay
+        self.lr = lr
+        # Model parameters
+        self.n_transforms = n_transforms
+        self.temperature = temperature
+        self.trans_type = trans_type
+        self.cosim = nn.CosineSimilarity()
+        # Encoder and Transformation layers
+        self.enc_hidden_dims = enc_hidden_dims
+        self.trans_hidden_dims = trans_hidden_dims
+        if self.trans_hidden_dims[-1] != in_features:
+            self.trans_hidden_dims.append(in_features)
+        # Encoder
+        self.enc = nn.Sequential(
+            *create_network(self.in_features, self.enc_hidden_dims, bias=False)
+        )
+        # Transforms
+        self.masks = self._create_masks()
+
+    @staticmethod
+    def add_model_specific_args(parent_parser):
+        parser = parent_parser.add_argument_group("ntl")
+        parser.add_argument("--trans_hidden_dims", type=List[int])
+        parser.add_argument("--enc_hidden_dims", type=List[int])
+        parser.add_argument("--n_transforms", type=str, default=11, help="number of transformations")
+        parser.add_argument("--temperature", type=float, default=0.1)
+        parser.add_argument("--lr", type=float, default=1e-3)
+        parser.add_argument("--weight_decay", type=float, default=1e-4)
+
+        return parent_parser
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(
+            self.parameters(),
+            lr=self.lr,
+            weight_decay=self.weight_decay
+        )
+        scheduler = StepLR(optimizer, step_size=20, gamma=0.9)
+        return optimizer #[optimizer], [scheduler]
+
+    def _create_masks(self):
+        masks = nn.ModuleList()
+        for k_i in range(self.n_transforms):
+            layers = create_network(self.in_features, self.trans_hidden_dims, bias=False)
+            layers.append(nn.Sigmoid())
+            masks.append(
+                nn.Sequential(*layers)
+            )
+        return masks
+
+    def forward(self, X: torch.Tensor, **kwargs):
+        X, y_true, full_labels = X
+        X = X.float()
+        scores = self.score(X)
+        return scores, y_true, full_labels
+
+    def training_step(self, batch, batch_idx):
+        X, _, _ = batch
+        X = X.float()
+        loss = self.score(X).mean()
+
+        return loss
+
+    def test_step(self, batch, batch_idx):
+        X, y_true, labels = batch
+        X = X.float()
+        scores = self.score(X)
+
+        return {
+            "scores": scores,
+            "y_true": y_true,
+            "labels": labels
+        }
+
+    def test_epoch_end(self, outputs) -> None:
+        scores, y_true, labels = np.array([]), np.array([]), np.array([])
+        for output in outputs:
+            scores = np.append(scores, output["scores"].cpu().detach().numpy())
+            y_true = np.append(y_true, output["y_true"].cpu().detach().numpy())
+            labels = np.append(labels, output["labels"].cpu().detach().numpy())
+        results, _ = metrics.score_recall_precision_w_threshold(scores, y_true)
+        for k, v in results.items():
+            self.log(k, v)
+
+    def score(self, X: torch.Tensor):
+        Xk = self._computeX_k(X)
+        Xk = Xk.permute((1, 0, 2))
+        Zk = self.enc(Xk)
+        Zk = F.normalize(Zk, dim=-1)
+        Z = self.enc(X)
+        Z = F.normalize(Z, dim=-1)
+        Hij = self._computeBatchH_ij(Zk)
+        Hx_xk = self._computeBatchH_x_xk(Z, Zk)
+
+        mask_not_k = (~torch.eye(self.n_transforms, dtype=torch.bool, device=self.device)).float()
+        numerator = Hx_xk
+        denominator = Hx_xk + (mask_not_k * Hij).sum(dim=2)
+        scores_V = numerator / denominator
+        score_V = (-torch.log(scores_V)).sum(dim=1)
+
+        return score_V
+
+    def _computeH_ij(self, Z):
+        hij = F.cosine_similarity(Z.unsqueeze(1), Z.unsqueeze(0), dim=2)
+        exp_hij = torch.exp(
+            hij / self.temperature
+        )
+        return exp_hij
+
+    def _computeBatchH_ij(self, Z):
+        hij = F.cosine_similarity(Z.unsqueeze(2), Z.unsqueeze(1), dim=3)
+        exp_hij = torch.exp(
+            hij / self.temperature
+        )
+        return exp_hij
+
+    def _computeH_x_xk(self, z, zk):
+        hij = F.cosine_similarity(z.unsqueeze(0), zk)
+        exp_hij = torch.exp(
+            hij / self.temperature
+        )
+        return exp_hij
+
+    def _computeBatchH_x_xk(self, z, zk):
+        hij = F.cosine_similarity(z.unsqueeze(1), zk, dim=2)
+        exp_hij = torch.exp(
+            hij / self.temperature
+        )
+        return exp_hij
+
+    def _computeX_k(self, X):
+        X_t_s = []
+
+        def transform(trans_type, x):
+            if trans_type == 'res':
+                return lambda mask, X: mask(X) + X
+            else:
+                return lambda mask, X: mask(X) * X
+
+        t_function = transform(self.trans_type, X)
+        for k in range(self.n_transforms):
+            X_t_k = t_function(self.masks[k], X)
+            X_t_s.append(X_t_k)
+        X_t_s = torch.stack(X_t_s, dim=0)
+
+        return X_t_s
+
+
 class LitGOAD(pl.LightningModule):
     def __init__(self,
                  in_features: int,
@@ -251,6 +386,21 @@ class LitGOAD(pl.LightningModule):
         )
         # Hypersphere centers
         self.centers = torch.zeros((self.feature_dim, self.n_transforms), device=self.device)
+
+    @staticmethod
+    def add_model_specific_args(parent_parser):
+        parser = parent_parser.add_argument_group("goad")
+        parser.add_argument("--n_transforms", type=str, default=11, help="number of transformations")
+        parser.add_argument("--feature_dim", type=int, help="output dimension of transformation")
+        parser.add_argument("--n_layers", type=int, help="number of layers")
+        parser.add_argument("--num_hidden_nodes", type=int, help="number of hidden nodes")
+        parser.add_argument("--enc_hidden_dims", type=str, default="64,64,64,64,32")
+        parser.add_argument("--lr", type=float, default=1e-3)
+        parser.add_argument("--weight_decay", type=float, default=1e-4)
+        parser.add_argument("--eps", type=float, default=0.)
+        parser.add_argument("--lamb", type=float, default=0.1)
+
+        return parent_parser
 
     def build_network(self):
         trunk_layers = [
