bad0d26e8459084ba45e21918947b8a568cd02f6
Jean-Charles Verdier <jean-charles.verdier@usherbrooke.ca>
2022-07-27 10:57:25 -0400

    added training scripts for IDS2017 and fixed misconfigurations on IDS2017 for AutoEncoder and MemAE.

---
+++ scripts/ids2017_train.ps1
@@ -0,0 +1,12 @@
+conda activate anomaly_detection &&
+cd .. &&
+python -m pyad.lightning_cli --config models/IDS2017/_data.yaml --config models/IDS2017/_trainer.yaml --config models/IDS2017/alad.yaml --save_dir experiments/training --n_runs 20 &&
+python -m pyad.lightning_cli --config models/IDS2017/_data.yaml --config models/IDS2017/_trainer.yaml --config models/IDS2017/autoencoder.yaml --save_dir experiments/training --n_runs 20 &&
+python -m pyad.lightning_cli --config models/IDS2017/_data.yaml --config models/IDS2017/_trainer.yaml --config models/IDS2017/dagmm.yaml --save_dir experiments/training --n_runs 20 &&
+python -m pyad.lightning_cli --config models/IDS2017/_data.yaml --config models/IDS2017/_trainer.yaml --config models/IDS2017/deepsvdd.yaml --save_dir experiments/training --n_runs 20 &&
+python -m pyad.lightning_cli --config models/IDS2017/_data.yaml --config models/IDS2017/_trainer.yaml --config models/IDS2017/drocc.yaml --save_dir experiments/training --n_runs 20 &&
+python -m pyad.lightning_cli --config models/IDS2017/_data.yaml --config models/IDS2017/_trainer.yaml --config models/IDS2017/dsebm-e.yaml --save_dir experiments/training --n_runs 20 &&
+python -m pyad.lightning_cli --config models/IDS2017/_data.yaml --config models/IDS2017/_trainer.yaml --config models/IDS2017/dsebm-r.yaml --save_dir experiments/training --n_runs 20 &&
+python -m pyad.lightning_cli --config models/IDS2017/_data.yaml --config models/IDS2017/_trainer.yaml --config models/IDS2017/goad.yaml --save_dir experiments/training --n_runs 20 &&
+python -m pyad.lightning_cli --config models/IDS2017/_data.yaml --config models/IDS2017/_trainer.yaml --config models/IDS2017/memae.yaml --save_dir experiments/training --n_runs 20 &&
+python -m pyad.lightning_cli --config models/IDS2017/_data.yaml --config models/IDS2017/_trainer.yaml --config models/IDS2017/neutralad.yaml --save_dir experiments/training --n_runs 20
