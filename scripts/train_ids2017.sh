8f4f56d0caf987b4c55c61431f0e4acb4bc31649
Jean-Charles Verdier <jean-charles.verdier@usherbrooke.ca>
2022-07-28 14:21:07 -0400

    update to IDS2017 and IDS2018 training scripts

--- scripts/train_ids2017.sh
+++ scripts/train_ids2017.sh
@@ -1,13 +1,13 @@
 cd .. &&
 eval "$(conda shell.bash hook)" &&
 conda activate $AD_CONDA_ENV &&
-python -m pyad.lightning_cli --config models/IDS2017/_data.yaml --config models/IDS2017/_trainer.yaml --config models/IDS2017/alad.yaml --save_dir experiments/training --n_runs 20 &&
-python -m pyad.lightning_cli --config models/IDS2017/_data.yaml --config models/IDS2017/_trainer.yaml --config models/IDS2017/autoencoder.yaml --save_dir experiments/training --n_runs 20 &&
-python -m pyad.lightning_cli --config models/IDS2017/_data.yaml --config models/IDS2017/_trainer.yaml --config models/IDS2017/dagmm.yaml --save_dir experiments/training --n_runs 20 &&
-python -m pyad.lightning_cli --config models/IDS2017/_data.yaml --config models/IDS2017/_trainer.yaml --config models/IDS2017/deepsvdd.yaml --save_dir experiments/training --n_runs 20 &&
-python -m pyad.lightning_cli --config models/IDS2017/_data.yaml --config models/IDS2017/_trainer.yaml --config models/IDS2017/drocc.yaml --save_dir experiments/training --n_runs 20 &&
-python -m pyad.lightning_cli --config models/IDS2017/_data.yaml --config models/IDS2017/_trainer.yaml --config models/IDS2017/dsebm-e.yaml --save_dir experiments/training/dsebm-e --n_runs 20 &&
-python -m pyad.lightning_cli --config models/IDS2017/_data.yaml --config models/IDS2017/_trainer.yaml --config models/IDS2017/dsebm-r.yaml --save_dir experiments/training/dsebm-r --n_runs 20 &&
-python -m pyad.lightning_cli --config models/IDS2017/_data.yaml --config models/IDS2017/_trainer.yaml --config models/IDS2017/goad.yaml --save_dir experiments/training --n_runs 20 &&
-python -m pyad.lightning_cli --config models/IDS2017/_data.yaml --config models/IDS2017/_trainer.yaml --config models/IDS2017/memae.yaml --save_dir experiments/training --n_runs 20 &&
-python -m pyad.lightning_cli --config models/IDS2017/_data.yaml --config models/IDS2017/_trainer.yaml --config models/IDS2017/neutralad.yaml --save_dir experiments/training --n_runs 20
+python -m pyad.lightning_cli --config models/IDS2017/_data.yaml --config models/IDS2017/_trainer.yaml --config models/IDS2017/alad.yaml --save_dir experiments/training --n_runs 5 &&
+python -m pyad.lightning_cli --config models/IDS2017/_data.yaml --config models/IDS2017/_trainer.yaml --config models/IDS2017/autoencoder.yaml --save_dir experiments/training --n_runs 5 &&
+python -m pyad.lightning_cli --config models/IDS2017/_data.yaml --config models/IDS2017/_trainer.yaml --config models/IDS2017/dagmm.yaml --save_dir experiments/training --n_runs 5 &&
+python -m pyad.lightning_cli --config models/IDS2017/_data.yaml --config models/IDS2017/_trainer.yaml --config models/IDS2017/deepsvdd.yaml --save_dir experiments/training --n_runs 5 &&
+python -m pyad.lightning_cli --config models/IDS2017/_data.yaml --config models/IDS2017/_trainer.yaml --config models/IDS2017/drocc.yaml --save_dir experiments/training --n_runs 5 &&
+python -m pyad.lightning_cli --config models/IDS2017/_data.yaml --config models/IDS2017/_trainer.yaml --config models/IDS2017/dsebm-e.yaml --save_dir experiments/training/dsebm-e --n_runs 5 &&
+python -m pyad.lightning_cli --config models/IDS2017/_data.yaml --config models/IDS2017/_trainer.yaml --config models/IDS2017/dsebm-r.yaml --save_dir experiments/training/dsebm-r --n_runs 5 &&
+python -m pyad.lightning_cli --config models/IDS2017/_data.yaml --config models/IDS2017/_trainer.yaml --config models/IDS2017/goad.yaml --save_dir experiments/training --n_runs 5 &&
+python -m pyad.lightning_cli --config models/IDS2017/_data.yaml --config models/IDS2017/_trainer.yaml --config models/IDS2017/memae.yaml --save_dir experiments/training --n_runs 5 &&
+python -m pyad.lightning_cli --config models/IDS2017/_data.yaml --config models/IDS2017/_trainer.yaml --config models/IDS2017/neutralad.yaml --save_dir experiments/training --n_runs 5
